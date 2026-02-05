2e80ba87c3d5765e1e88ca2da4bfaae64f0a3fab
senolbozali-dot <senolbozali@gmail.com>
2026-02-06 01:19:40 +0300

    Delete env.js

--- backendcode/src/config/env.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-console.log(process.env.NODE_ENV);
-export const env = {
-  port: process.env.PORT || 3000,
-  nodeEnv: process.env.NODE_ENV || "development",
-  mongo_url:process.env.MONGO_URI
-};
